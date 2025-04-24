a4b6a7e492434a0bcad6e36f95eaf3d94cb6e774
RaymondChen_Viate <93709736+310652027@users.noreply.github.com>
2025-04-24 10:03:35 +0800

    Add files via upload

---
+++ Graph/Class2509.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTR
+{
+    internal class Class2509
+    {
+        public int[] CycleLengthQueries(int n, int[][] queries)
+        {
+            int[] Ans = new int[queries.Length];
+
+            for (int index = 0; index < queries.Length; index++)
+            {
+                int[] query = queries[index];
+                int select1 = query[0], select2 = query[1];
+                //Find Lowest Common Ancestor
+                int path = 0;
+                while(select1 != select2)
+                {
+                    if (select1 > select2) select1 >>= 1;
+                    else select2 >>= 1;
+                    path++;
+                }
+                Ans[index] = path + 1;
+            }
+            return Ans;
+        }
+    }
+}
