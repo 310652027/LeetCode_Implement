6a87b590cb9c7fec733e7e7cbfac00dded7c5987
RaymondChen_Viate <93709736+310652027@users.noreply.github.com>
2025-04-28 09:48:00 +0800

    Add files via upload

---
+++ 2302_CountSubarraysWithScoreLessThanK.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTR
+{
+    internal class _2302_CountSubarraysWithScoreLessThanK
+    {
+        public long CountSubarrays(int[] nums, long k)
+        {
+            // sliding window to count the number of the array contain num at index i 
+            long left = 0, right = 0;
+            long sum = 0;
+            long times = 0;
+            while (right != nums.Length)
+            {
+                sum += nums[right++];
+                //score can used a better calculation which only used +/- , but also sum * len is more trivial.
+                while (sum * (right - left) >= k) sum -= nums[left++];
+                // form index left to index right are all subarray which score is smaller than k. Just add it in result.
+                times += right - left;
+            }
+            return times;
+        }
+    }
+}
