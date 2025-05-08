46fdd405476a0e10fba5e534127758e43261d517
RaymondChen_Viate <93709736+310652027@users.noreply.github.com>
2025-05-08 13:57:40 +0800

    Update and rename 1998_GCDSortofanArray.cs to UnionFind/1998_GCDSortofanArray.cs

--- UnionFind/1998_GCDSortofanArray.cs
+++ UnionFind/1998_GCDSortofanArray.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,24 +13,23 @@ namespace HTR
             int numMax = nums[0];
             foreach (int num in nums) numMax = int.Max(num, numMax);
 
+            // Build the Parent Tabel , Constructing the Method "find" and "Union"
+            // Union Will find the primes which can divide the num, then the group of primes will be Union 
             int[] Parent = new int[numMax + 1];
             for (int i = 2; i <= numMax; i++) Parent[i] = i;
-            int find(int x)          // ★ 路徑壓縮
-            {
-                return Parent[x] == x ? x : Parent[x] = find(Parent[x]);
-            }
+            int find(int x) => Parent[x] == x ? x : Parent[x] = find(Parent[x]);
             void Union(int num)
             {
                 int x = num;
                 List<int> primes = new List<int>();
 
-                if (x % 2 == 0)                  // 取因子 2
+                if (x % 2 == 0)
                 {
                     primes.Add(2);
                     while (x % 2 == 0) x >>= 1;
                 }
 
-                for (int p = 3; p * p <= x; p += 2) // 只到 sqrt(x)
+                for (int p = 3; p * p <= x; p += 2)
                 {
                     if (x % p == 0)
                     {
@@ -38,9 +37,9 @@ namespace HTR
                         while (x % p == 0) x /= p;
                     }
                 }
-                if (x > 1) primes.Add(x);        // 剩餘質因子
                 primes.Add(num);
-                /*  依序 union 所有質因子  */
+                if (x > 1) primes.Add(x);
+
                 int root = find(primes[0]);
                 for (int i = 1; i < primes.Count; i++)
                     Parent[find(primes[i])] = root;
@@ -51,8 +50,11 @@ namespace HTR
             int[] sorted = nums.ToArray();
             Array.Sort(sorted);
 
+
+            //After Unions the primes to same group, check each element can change with the element ont index which standing on the orignal position.
             for (int i = 0; i < nums.Length; i++)
-                if (find(nums[i]) != find(sorted[i])) return false;
+                if (find(nums[i]) != find(sorted[i]))
+                    return false;
             return true;
         }
     }
