9dfbf1a530600a5150c04ef88710ee094bd36c3c
RaymondChen_Viate <93709736+310652027@users.noreply.github.com>
2025-06-10 13:40:38 +0800

    Add files via upload

---
+++ 2376_CountSpecialIntegers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Emgu.Util.Platform;
+
+namespace HTR
+{
+    internal class _2376_CountSpecialIntegers
+    {
+        public int CountSpecialNumbers(int n)
+        {
+            char[] nSpilt = n.ToString().ToCharArray();
+            int totalIndex = nSpilt.Length;
+            int AllMask = 0;
+            Dictionary<long, int> FindingResult = new Dictionary<long, int>();
+
+            int DigitDp(int zeroTrail, int UpperBound, int index, int num)
+            {
+                long key = (index << 12) | (UpperBound << 11) | (zeroTrail << 10) | AllMask;
+
+                if (FindingResult.ContainsKey(key)) return FindingResult[key];
+                if (index == totalIndex) return zeroTrail == 1 ? 0 : 1;
+
+                int count = 0;
+                int upperbound = UpperBound == 1 ? nSpilt[index] - '0' : 9;
+                for (int i = 0; i <= upperbound; i++)
+                {
+                    if (zeroTrail == 1 && i == 0)
+                    {
+                        int N_zeroTrail = 1;
+                        int N_UpperBound = (UpperBound == 1 && i == upperbound) ? 1 : 0;
+                        count += DigitDp(N_zeroTrail, N_UpperBound, index + 1, num * 10);
+                        continue;
+                    }
+
+                    if (((AllMask >> i) & 1) == 0)
+                    {
+                        AllMask += (1 << i);
+                        int N_zeroTrail = 0;
+                        int N_UpperBound = (UpperBound == 1 && i == upperbound) ? 1 : 0;
+                        count += DigitDp(N_zeroTrail, N_UpperBound, index + 1, num * 10 + i);
+                        AllMask -= (1 << i);
+                    }
+                }
+                FindingResult[key] = count;
+                return count;
+            }
+            return DigitDp(1, 1, 0, 0);
+        }
+    }
+}
---
+++ 440_KthSmallestinLexicographicalOrder.cs
@@ -0,0 +1,51 @@
+﻿using Emgu.CV.Flann;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTR
+{
+    class _440_KthSmallestinLexicographicalOrder
+    {
+        public int FindKthNumber(int n, int k)
+        {
+            // the Subfunction will find how many element contain in range [prefix, next) 
+            long Counting_Range(long prefix, long next)
+            {
+                long count = 0;
+                while(prefix <= n)
+                {
+                    count += Math.Min(n + 1L, next) - prefix;
+                    prefix *= 10;
+                    next *= 10;
+                }
+                return count;
+            }
+
+            long prefix = 1;
+            k--;
+            while(k > 0)
+            {
+                //Every time we loop, we find the count from [i , i+1) ,
+                // that's , how many element using same prefix .
+                long cnt = Counting_Range(prefix, prefix + 1);
+                if(k >= cnt)
+                {
+                    // the count of element is too small, this region doesn' have the result we want.
+                    k -= (int)cnt;
+                    prefix++;
+                }
+                else
+                {
+                    // the element contain the result we desire, need to go deeper to fine it
+                    prefix *= 10;
+                    k--;
+                }
+            }
+            return (int)prefix;
+        }
+    }
+}
